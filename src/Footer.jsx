6d3557b55f27c8c1ff7255b470f90b6bf1826c78
david-capek <david.capek@maiven.io>
2025-10-01 18:57:01 +0200

    update 2

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,30 @@
-function Footer(){
-return(
-    <>
-        <footer className="container-footer">
-            <img src="lum re.sve" alt="" />
-        </footer>
-    </>
-);
+import lumiarelogo from './assets/lumiare.svg'
+import IA from './assets/IA.svg'
+import Maiven from './assets/Maiven.png'
+
+function Footer() {
+  return (
+    <footer className="container-footer">
+      <div className="lumiare-logo">
+        <img src={lumiarelogo} alt="Lumiare logo" className="lumiare-base" />
+        <img src={IA} alt="IA" className="ia-logo" />
+        <p className='createdby'>Created by</p>
+        <img src={Maiven} alt="" className='maiven-image'/>
+      </div>
+      <p className='copyright'>© Lumiare™ 2025. All Rights Reserved. </p>
+      <div className='info'>
+        <p>Home</p>
+        <p>Solutions</p>
+        <p>Pricing</p>
+        <p>About us</p>
+        <p>Contact</p>
+      </div>
+      <div className='terms'>
+        <p>Privacy policy</p>
+        <p>Terms</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
