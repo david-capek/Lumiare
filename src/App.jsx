daff18bcef00a63bae742c5d19c4fd86b35ddc9c
david-capek <david.capek@maiven.io>
2025-10-02 20:11:47 +0200

    update 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import Waitlist from './Waitlist'
 
 function App() {
 
   return (
     <>
 <Header/>
+<Waitlist/>
 <Footer/>
     </>
   )
--- src/Waitlist.jsx
+++ src/Waitlist.jsx
@@ -1,5 +1,17 @@
-function Waitlist(){
+import Rocket from './assets/rocket.svg';
 
+function Waitlist(){
+return(
+<div className='container-waitlist'>
+    <img src={Rocket} alt="Rocket" className='rocket-image'/>
+    <div className='ellipse'>
+        <div className='e1'></div>
+        <div className='e2'></div>
+        <div className='e3'></div>
+        <div className='e4'></div>
+    </div>
+</div>
+);
 }
 
 export default Waitlist;
