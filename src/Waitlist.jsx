160a6bdaaa7fd4642d984d7fd3ec2db89e1cc0ac
david-capek <david.capek@maiven.io>
2025-10-03 16:09:21 +0200

    update 5

--- src/Waitlist.jsx
+++ src/Waitlist.jsx
@@ -1,4 +1,6 @@
 import Rocket from './assets/rocket.svg';
+import Star1 from './assets/star1.svg';
+import Star2 from './assets/star2.svg';
 
 function Waitlist(){
 return(
@@ -10,6 +12,10 @@ return(
         <div className='e3'></div>
         <div className='e4'></div>
     </div>
+    <p className='title'>Stop Guessing <br/><span style={{fontWeight:600}}>Start Delivering</span></p>
+    <p className='quote'>With Lumiare.ai, you can turn chaos into clarity, build trust with clients, and deliver success every time.</p>
+    <img src={Star1} alt="" style={{ position: "absolute", left: "7%", top: "68%" }}/>
+    <img src={Star2} alt="" style={{ position: "absolute", left: "80%", top: "45%" }}/>
 </div>
 );
 }
