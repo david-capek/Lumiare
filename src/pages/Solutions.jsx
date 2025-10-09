424984db4b7e748ec1416d014911f1f7d707caf0
david-capek <david.capek@maiven.io>
2025-10-09 18:38:58 +0200

    update 9

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -1,7 +1,9 @@
 import Footer from "../Footer";
 import Header2 from "../Header2";
-import Girl from '../assets/girl.svg'
+import Girl from '../assets/girl.svg';
 import { useLayoutEffect } from "react";
+import Message from "../assets/message.svg";
+import SOW from '../assets/sow.svg';
 
 export default function Solutions() {
     useLayoutEffect(() => {
@@ -26,6 +28,14 @@ export default function Solutions() {
             <Header2/>
             <div className='c1'> 
                 <img src={Girl} alt="girl" className="girl-img"/>
+                <div className="quote-container">
+                    <p className="quote3">From Chaos<br/><span style={{fontWeight:"600"}}>to a Master Plan</span> </p>
+                    <p className="text">A comprehensive, end-to-end workflow designed for professional services companies like yours. Lumiare.ai is a complete workflow for your most critical projects, giving your team superhuman precision and efficiency.</p>
+                    <p className="btn-start">Start for Free</p>
+                    <p className="btn-request">Request a Demo</p>
+                </div>
+                <img src={Message} alt="" className="message"/>
+                <img src={SOW} alt="" className="sow"/>
             </div>
             <Footer/>
         </>
